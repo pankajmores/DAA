5dbff525480c08305c4ca5d64b6b0c6b2f7bf400
pankajmores <136971374+pankajmores@users.noreply.github.com>
2025-04-24 21:24:46 +0530

    Update assi 6.cpp

--- DAA/assi 6.cpp
+++ DAA/assi 6.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[101][1001]; // assuming n <= 100 and W <= 1000
 
-int knapsack(int W, vector<int> &wt, vector<int> &val, int n) {
-   vector<vector<int> > dp(n + 1, vector<int>(W + 1, 0));
-
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i <= n; i++) {
         for (int w = 0; w <= W; w++) {
-            if (wt[i - 1] <= w){
+            if (i == 0 || w == 0) {
+                dp[i][w] = 0;
+            } else if (wt[i - 1] <= w) {
                 dp[i][w] = max(dp[i - 1][w], val[i - 1] + dp[i - 1][w - wt[i - 1]]);
-            }
-            else{
+            } else {
                 dp[i][w] = dp[i - 1][w];
             }
         }
@@ -23,15 +22,16 @@ int main() {
     int n, W;
     cout << "Number of items: ";
     cin >> n;
-    
-    vector<int> wt(n), val(n);
+
+    int wt[100], val[100]; // assuming max 100 items
+
     cout << "Enter weight and value of each item:\n";
     for (int i = 0; i < n; i++)
         cin >> wt[i] >> val[i];
-    
+
     cout << "Enter knapsack capacity: ";
     cin >> W;
-    
+
     cout << "Max value: " << knapsack(W, wt, val, n) << endl;
     return 0;
 }
